46faf8fe4a8f42d4989c3f50b6092e857d68a3d1
Fabian Ahammer <f.ahammer@students.htl-leonding.ac.at>
2019-12-04 21:28:09 +0100

    Initiialize

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# Advanced Vue.js Component Composition With Container Components
-
-[![Patreon](https://img.shields.io/badge/patreon-donate-blue.svg)](https://www.patreon.com/maoberlehner)
-[![Donate](https://img.shields.io/badge/Donate-PayPal-blue.svg)](https://paypal.me/maoberlehner)
+# Cypress Vuejs
 
 This is an example project for the following article: [Advanced Vue.js Component Composition With Container Components](https://markus.oberlehner.net/blog/advanced-vue-component-composition-with-container-components/)
 
@@ -17,24 +14,9 @@ npm run serve
 # Build for production with minification.
 npm run build
 
-# Run unit tests.
-npm run test:unit
-
 # Run acceptance tests.
 npm run test:acceptance
 ```
 
-## About
+[Creator]( https://twitter.com/MaOberlehner )
 
-### Author
-
-Markus Oberlehner  
-Website: https://markus.oberlehner.net  
-Twitter: https://twitter.com/MaOberlehner  
-PayPal.me: https://paypal.me/maoberlehner  
-Patreon: https://www.patreon.com/maoberlehner
-
-### License
-
-MIT
-"# SypCypressVue" 
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,59 @@
+<template>
+  <div :class="$style.wrap">
+    <div :class="$style.intro">
+      <h1 :class="$style.headline">
+        SYP Test Site
+      </h1>
+      <p>
+        Demo application showing the use of Cypress (E2E Testing), with CICD
+      </p>
+    </div>
+
+    <div :class="$style.teaser">
+      <h2 :class="$style.headline">
+        Products
+      </h2>
+      <ProductTeaserContainer />
+    </div>
+
+    <div :class="$style.teaser">
+      <h2 :class="$style.headline">
+        Articles
+      </h2>
+      <ArticleTeaserContainer />
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticleTeaserContainer from "../components/ArticleTeaserContainer.vue";
+import ProductTeaserContainer from "../components/ProductTeaserContainer.vue";
+
+export default {
+  name: `PageHome`,
+  components: {
+    ArticleTeaserContainer,
+    ProductTeaserContainer
+  }
+};
+</script>
+
+<style lang="scss" module>
+.intro {
+  max-width: 32em;
+  margin-right: auto;
+  margin-left: auto;
+  font-size: 1.25em;
+  text-align: center;
+}
+
+.headline {
+  margin-bottom: 0.5em;
+  font-size: 1.75em;
+  line-height: 1.2;
+}
+
+.teaser {
+  margin-top: 3em;
+}
+</style>
